74d5d33feea70c654aefdc3c30f191b29ff9474d
ggroshansii <garthgroshansii@gmail.com>
2022-04-19 21:24:47 -0400

    added blog app urls and views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse 
 
 # Create your views here.
+
+blog_posts = {
+    "gardening_tools": "blog post about gardening tools",
+    "soil_amendments": "add these things to your soil: lime, gypsum, organic matter"
+}
+
+def index(request):
+    return HttpResponse("homepage")
+
+def posts(request):
+    return HttpResponse("all posts")
+
+def post_detail(request, post_detail):
+    return HttpResponse(blog_posts[post_detail])
