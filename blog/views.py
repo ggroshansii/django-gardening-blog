7c19c7fff1f211a00814ab8aa4769770b0425ce6
ggroshansii <garthgroshansii@gmail.com>
2022-05-09 22:53:49 -0400

    add dummy data to blog views

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,51 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse 
+from datetime import date
 
 # Create your views here.
 
-blog_posts = {
-    "gardening_tools": "blog post about gardening tools",
-    "soil_amendments": "add these things to your soil: lime, gypsum, organic matter"
-}
+blog_posts = [
+    {
+        "slug": "flowering-garden",
+        "image": "gardening-1.jpg",
+        "author": "Garth",
+        "date": date(2022, 5, 12),
+        "title": "Types of Soil",
+        "excerpt" : "There are 12 different soil orders in the USA",
+        "content": """
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        """
+    },
+    {
+        "slug": "flowering-garden",
+        "image": "gardening-1.jpg",
+        "author": "Garth",
+        "date": date(2022, 5, 12),
+        "title": "Types of Soil",
+        "excerpt" : "There are 12 different soil orders in the USA",
+        "content": """
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        """
+    },
+    {
+        "slug": "flowering-garden",
+        "image": "gardening-1.jpg",
+        "author": "Garth",
+        "date": date(2022, 5, 12),
+        "title": "Types of Soil",
+        "excerpt" : "There are 12 different soil orders in the USA",
+        "content": """
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        """
+    },
+]
 
 def home_page(request):
     return render(request, "blog/index.html")
