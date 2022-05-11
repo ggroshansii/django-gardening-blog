d0f54a3f27d5a3a461c1f0cf843f39a26950eeb7
ggroshansii <garthgroshansii@gmail.com>
2022-05-10 22:45:49 -0400

    dynamically render blog_post object/dict in post.html

--- blog/views.py
+++ blog/views.py
@@ -11,8 +11,8 @@ blog_posts = [
         "image": "gardening-1.jpg",
         "author": "Garth",
         "date": date(2022, 5, 12),
-        "title": "Types of Soil",
-        "excerpt" : "There are 12 different soil orders in the USA",
+        "title": "Spring seeds",
+        "excerpt" : "You need to buy these seeds this Spring",
         "content": """
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
@@ -21,11 +21,11 @@ blog_posts = [
     },
     {
         "slug": "flowering-garden",
-        "image": "gardening-1.jpg",
+        "image": "gardening-2.jpg",
         "author": "Garth",
-        "date": date(2022, 5, 12),
-        "title": "Types of Soil",
-        "excerpt" : "There are 12 different soil orders in the USA",
+        "date": date(2022, 10, 21),
+        "title": "Winter Gardening",
+        "excerpt" : "Here are the best cover crops for your winter gardening",
         "content": """
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
@@ -34,11 +34,11 @@ blog_posts = [
     },
     {
         "slug": "flowering-garden",
-        "image": "gardening-1.jpg",
+        "image": "gardening-3.jpg",
         "author": "Garth",
-        "date": date(2022, 5, 12),
-        "title": "Types of Soil",
-        "excerpt" : "There are 12 different soil orders in the USA",
+        "date": date(2022, 2, 10),
+        "title": "Soil Amendments to buy",
+        "excerpt" : "Gypsum, lime, fertilizer, what you need and what you don't",
         "content": """
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
@@ -47,8 +47,16 @@ blog_posts = [
     },
 ]
 
+def get_date(post):
+    return post['date']
+
 def home_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(blog_posts, key=get_date)
+    print('hey', sorted_posts)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts,
+    })
 
 def posts(request):
     return render(request, "blog/posts.html")
